2e736894b822dc540d183a65eb36a0e7aef7b38b
AkshathKamath <akshathkamathwork@gmail.com>
2024-04-24 18:38:36 +0530

    Added Comments

--- loan_prediction.py
+++ loan_prediction.py
@@ -8,47 +8,51 @@ import joblib
 
 
 
-# load the dataset
-dataset = pd.read_csv("./Datasets/train.csv")
-numerical_cols = dataset.select_dtypes(include=['int64','float64']).columns.tolist()
-categorical_cols = dataset.select_dtypes(include=['object']).columns.tolist()
+## Load df
+df = pd.read_csv("./Datasets/train.csv")
+
+## Categorising into numerical & categorical features
+numerical_cols = df.select_dtypes(include=['int64','float64']).columns.tolist()
+categorical_cols = df.select_dtypes(include=['object']).columns.tolist()
 categorical_cols.remove('Loan_Status')
 categorical_cols.remove('Loan_ID')
 
-# Filling categorical columns with mode
+## Mode Imputation of Categorical cols
 for col in categorical_cols:
-    dataset[col].fillna(dataset[col].mode()[0], inplace=True)
+    df[col].fillna(df[col].mode()[0], inplace=True)
 
-# Filling Numerical columns with median
+## Median Imputation of Numerical cols
 for col in numerical_cols:
-    dataset[col].fillna(dataset[col].median(), inplace=True)
+    df[col].fillna(df[col].median(), inplace=True)
 
-# Take care of outliers
-dataset[numerical_cols] = dataset[numerical_cols].apply(lambda x: x.clip(*x.quantile([0.05, 0.95])))
+## Outliers Handling
+df[numerical_cols] = df[numerical_cols].apply(lambda x: x.clip(*x.quantile([0.05, 0.95])))
 
-# Log Transforamtion & Domain Processing
-dataset['LoanAmount'] = np.log(dataset['LoanAmount']).copy()
-dataset['TotalIncome'] = dataset['ApplicantIncome'] + dataset['CoapplicantIncome']
-dataset['TotalIncome'] = np.log(dataset['TotalIncome']).copy()
+## Log Transforamtion & Adding Income cols to single col
+df['LoanAmount'] = np.log(df['LoanAmount']).copy()
+df['TotalIncome'] = df['ApplicantIncome'] + df['CoapplicantIncome']
+df['TotalIncome'] = np.log(df['TotalIncome']).copy()
 
 
-# Dropping ApplicantIncome and CoapplicantIncome
-dataset = dataset.drop(columns=['ApplicantIncome','CoapplicantIncome'])
+## Dropping ApplicantIncome and CoapplicantIncome cols
+df = df.drop(columns=['ApplicantIncome','CoapplicantIncome'])
 
-# Label encoding categorical variables
+## Label encoding categorical features
 for col in categorical_cols:
     le = LabelEncoder()
-    dataset[col] = le.fit_transform(dataset[col])
+    df[col] = le.fit_transform(df[col])
 
-#Encode the target columns
-dataset['Loan_Status'] = le.fit_transform(dataset['Loan_Status'])
+## Encode target column
+df['Loan_Status'] = le.fit_transform(df['Loan_Status'])
 
-# Train test split
-X = dataset.drop(columns=['Loan_Status', 'Loan_ID'])
-y = dataset.Loan_Status
+## Split into independent & target features
+X = df.drop(columns=['Loan_Status', 'Loan_ID'])
+y = df.Loan_Status
 RANDOM_SEED = 6
 
-# RandomForest
+## Train-test split not needed as we build model on entire dataset and predict with values provided in input
+
+## RandomForest
 rf = RandomForestClassifier(random_state=RANDOM_SEED)
 param_grid_forest = {
     'n_estimators': [200,400, 700],
@@ -56,7 +60,7 @@ param_grid_forest = {
     'criterion' : ["gini", "entropy"],
     'max_leaf_nodes': [50, 100]
 }
-
+## Hyperparameter Tuning
 grid_forest = GridSearchCV(
         estimator=rf,
         param_grid=param_grid_forest, 
@@ -65,22 +69,25 @@ grid_forest = GridSearchCV(
         scoring='accuracy',
         verbose=0
     )
+## Creating the model
 model_forest = grid_forest.fit(X, y)
 
+## Saving the model
 joblib.dump(model_forest, './Trained-Models/RF_loan_model.joblib')
 
-loaded_model = joblib.load('./Trained-Models/RF_loan_model.joblib')
+# ## Loading the model
+# loaded_model = joblib.load('./Trained-Models/RF_loan_model.joblib')
 
-data = [[
-                1.0,
-                0.0,
-                0.0,
-                0.0,
-                0.0,
-                4.98745,
-                360.0,
-                1.0,
-                2.0,
-                8.698
-            ]]
-print(f"Prediction is : {loaded_model.predict(pd.DataFrame(data))}")
+# data = [[
+#                 1.0,
+#                 0.0,
+#                 0.0,
+#                 0.0,
+#                 0.0,
+#                 4.98745,
+#                 360.0,
+#                 1.0,
+#                 2.0,
+#                 8.698
+#             ]]
+# print(f"Prediction is : {loaded_model.predict(pd.DataFrame(data))}") ## Generate prediction to test
